dfa453e915d21875a3f271df22400b22acd095e8
jaaimino <Jacobaimino@gmail.com>
2016-05-05 13:16:52 -0400

    Huge performance improvements using ng-if and conditional rendering

--- app/create/create.controller.js
+++ app/create/create.controller.js
@@ -4,6 +4,9 @@ angular.module('jsdungeon')
   .controller('CreateCtrl', function ($scope, $http, JSDungeon) {
   	$scope.dungeon = JSDungeon.getDungeonTemplate();
   	$scope.disabled = true; //Set all ng-disabled to true by default
+  	$scope.saveTemplate = function(){
+  		JSDungeon.setDungeonTemplate($scope.dungeon);
+  	}
   	$scope.deleteRoom = function(key){
   		delete $scope.dungeon.rooms[key];
   		JSDungeon.setDungeonTemplate($scope.dungeon);
