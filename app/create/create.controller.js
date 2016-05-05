ea9e2c5b8d340b1ce86f949990b4baf6d865bb6e
jaaimino <jacobaimino@gmail.com>
2016-05-05 11:50:47 -0400

    Can edit items in rooms and in player starting inventory

--- app/create/create.controller.js
+++ app/create/create.controller.js
@@ -3,9 +3,9 @@
 angular.module('jsdungeon')
   .controller('CreateCtrl', function ($scope, $http, JSDungeon) {
   	$scope.dungeon = JSDungeon.getDungeonTemplate();
-  	$scope.deleteRoom = function(room){
-  		delete $scope.dungeon.rooms[room];
-  		console.log($scope.dungeon.rooms[room]);
+  	$scope.disabled = true; //Set all ng-disabled to true by default
+  	$scope.deleteRoom = function(key){
+  		delete $scope.dungeon.rooms[key];
   		JSDungeon.setDungeonTemplate($scope.dungeon);
   	}
   });
