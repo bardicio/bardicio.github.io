ad7fe1aa753abd749b1dad1995432044eee3a9bf
jaaimino <jacobaimino@gmail.com>
2016-05-05 01:00:28 -0400

    More work on editor

--- app/create/create.controller.js
+++ app/create/create.controller.js
@@ -3,4 +3,7 @@
 angular.module('jsdungeon')
   .controller('CreateCtrl', function ($scope, $http, JSDungeon) {
   	$scope.dungeon = JSDungeon.getDungeonTemplate();
+  	$scope.deleteRoom = function(room){
+  		delete $scope.dungeon.rooms[room];
+  	}
   });
